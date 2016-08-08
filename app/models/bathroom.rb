4f576eb335cff2505eada92c80eefd1cbfe3f289
tauan lemos <tauanmiguel@gmail.com>
2016-08-08 15:52:42 +0100

    Add bathroom model

---
+++ app/models/bathroom.rb
@@ -0,0 +1,4 @@
+class Bathroom < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :address, :accepted_gender
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808142200) do
+ActiveRecord::Schema.define(version: 20160808144341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bathrooms", force: :cascade do |t|
+    t.integer  "price"
+    t.text     "address"
+    t.string   "accepted_gender"
+    t.string   "type"
+    t.boolean  "towel",           default: false
+    t.boolean  "shower_gel",      default: false
+    t.boolean  "shampoo",         default: false
+    t.integer  "user_id"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "bathrooms", ["user_id"], name: "index_bathrooms_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -37,4 +52,5 @@ ActiveRecord::Schema.define(version: 20160808142200) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "bathrooms", "users"
 end
