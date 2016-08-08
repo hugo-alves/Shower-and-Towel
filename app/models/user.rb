93156cb1f6975417405d1c2f82039448cd796cbf
tauan lemos <tauanmiguel@gmail.com>
2016-08-08 18:11:44 +0100

    Add navbar with login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates_presence_of :name, :gender
+  # validates_presence_of :name, :gender
   validates :email, presence: true, uniqueness: true
   has_many :bathrooms
   devise :database_authenticatable, :registerable,
