98371097ffa356796a6e36a336a61a550a5ecbd7
CarlosGomes94290 <madureiracharles@gmail.com>
2016-08-09 17:33:00 +0100

    changed buttons signup and hidde form for email sign up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+$(document).ready(function() {
+$('.togglebutton').click(function() {
+$('.form-inputs').toggleClass('hidden');
+$('.form-actions').toggleClass('hidden');
+});
+});
