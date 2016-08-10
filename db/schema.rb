656646be21033271a39bd2058b7deb1d3bc4cbe9
Hugo Alves <goninja@gmail.com>
2016-08-10 16:24:00 +0100

    show page bath prettier and added new columns do bath table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810131853) do
+ActiveRecord::Schema.define(version: 20160810142712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20160810131853) do
     t.datetime "updated_at",                      null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.text     "description"
+    t.string   "picture"
   end
 
   add_index "bathrooms", ["user_id"], name: "index_bathrooms_on_user_id", using: :btree
