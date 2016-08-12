562a70a9a7f54f38575e963acd192685ee306aa4
CarlosGomes94290 <madureiracharles@gmail.com>
2016-08-12 12:24:05 +0100

    add index and show pages for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,21 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
     @booking = Booking.new
   end
+
   def create
     @bathroom = Bathroom.find(params[:bathroom_id])
     @booking = current_user.bookings.create(booking_params.merge(bathroom_id: @bathroom.id, user_id: current_user.id, price: @bathroom.price, book_time: params[:booking][:bookings][:book_time] ))
-    
+
     redirect_to bathroom_path(@bathroom.id), notice: "Your booking has been created..."
   end
 
